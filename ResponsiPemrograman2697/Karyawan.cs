962ef06d3ced5f6e02c4a1d1e9b64d7d1abf04dd
Lincchi <caroline.lincchi@gmail.com>
2020-04-14 10:33:31 +0700

    Responsi

---
+++ ResponsiPemrograman2697/Karyawan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrograman2697
+{
+    class Karyawan
+    {
+             
+        //contructor dengan 3 parameter
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+            
+
+            if (gajibulanan<0)
+            {
+                gajibulanan = 0;
+            }
+            else
+            {
+                gajibulanan = gajibulanan;
+            }
+
+            Nik = nik;
+            Nama = nama;
+            GajiBulanan = gajibulanan;
+            
+            int kenaikan = gajibulanan * 10 / 100;
+            int gaji = kenaikan + gajibulanan;
+
+            Gaji = gaji;
+        }
+
+        
+
+        //properties
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public int Gaji { get; set; }
+
+        public void CetakGaji()
+        {
+           Console.WriteLine("{0} {1}       {2}", Nik, Nama, GajiBulanan);
+        }
+
+        public void CetakKenaikan()
+        {
+            Console.WriteLine("{0} {1}       {2}", Nik, Nama, Gaji);
+        }
+
+    }
+}
---
+++ ResponsiPemrograman2697/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ResponsiPemrograman2697
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek karyawan1
+
+            header();
+
+            Karyawan karyawan1 = new Karyawan("190302123", "Paijo", -90000000);
+            Console.Write("1. ");
+            karyawan1.CetakGaji();
+
+            //membuat objek Karyawan2
+            Karyawan karyawan2 = new Karyawan("190302124", "Jono", 2000000);
+            Console.Write("2. ");
+            karyawan2.CetakGaji();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Asyique ada kenaikan gaji 10%");
+            Console.WriteLine();
+            header();
+            Console.Write("1. ");
+            karyawan1.CetakKenaikan();
+            Console.Write("2. ");
+            karyawan2.CetakKenaikan();
+
+        }
+
+
+       static void header()
+        {
+            Console.WriteLine("No NIK/Nama                 Gaji");
+            Console.WriteLine("--------------------------------");
+           
+        }
+       
+    }
+    
+}
